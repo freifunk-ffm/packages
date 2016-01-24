3b98a4850a9d8608701852cfb19cd8abf2235f42
JuS <sagurna@s3i.de>
2016-01-24 21:13:07 +0100

    Revert "adaptions for mtu-setting"
    This reverts commit 353a435f0c13998c06915a2523e04d55a7394166.

--- ffffm/ffffm-luci-mesh-vpn-fastd/files/usr/lib/lua/luci/model/cbi/admin/mesh_vpn_fastd.lua
+++ ffffm/ffffm-luci-mesh-vpn-fastd/files/usr/lib/lua/luci/model/cbi/admin/mesh_vpn_fastd.lua
@@ -6,7 +6,7 @@ f.template = "admin/expertmode"
 
 local s = f:section(SimpleSection)
 
-local o = s:option(Value, 'mtu')
+local o = s:option(Value, 'mode')
 o.template = "gluon/cbi/mesh-vpn-fastd-mode"
 
 local mtu = uci:get('fastd', 'mesh_vpn', 'mtu')
@@ -19,12 +19,16 @@ end
 function f.handle(self, state, data)
   if state == FORM_VALID then
     local site = require 'gluon.site_config'
-local mtuval
+
     local methods = {}
-    if data.mtu == '1426' then
-	    mtuval = 1426
-    else
-	    mtuval = 1280
+    if data.mode == 'performance' then
+      table.insert(methods, 'null')
+    end
+
+    for _, method in ipairs(site.fastd_mesh_vpn.methods) do
+      if method ~= 'null' then
+	table.insert(methods, method)
+      end
     end
 
     uci:set('fastd', 'mesh_vpn', 'mtu', mval)
