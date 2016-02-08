e3143e7efcfceefb1e48e50ccc7c8b5cdffcc5f6
JuS <sagurna@s3i.de>
2016-02-08 14:38:14 +0100

    ffffm-luci-set-mtu: Neues Flagauto_mtu_enabled hinzu, und Robustheit erhöht.

--- ffffm/ffffm-luci-set-mtu/files/usr/lib/lua/luci/model/cbi/admin/setmtu.lua
+++ ffffm/ffffm-luci-set-mtu/files/usr/lib/lua/luci/model/cbi/admin/setmtu.lua
@@ -4,8 +4,15 @@ local i18n = require "luci.i18n"
 local uci = luci.model.uci.cursor()
 local config = 'fastd'
 
--- where to read the configuration from
-local mtu = uci:get(config, "mesh_vpn", "mtu")
+
+-- Auto or where to read the configuration from
+
+if uci:get(config, "mesh_vpn", "auto_mtu_enabled") == '1' then
+  mtu = 'auto'
+else
+  mtu = uci:get(config, "mesh_vpn", "mtu")
+end
+
 
 f = SimpleForm("mtu", i18n.translate("MTU-Config-Titel"))
 f.template = "admin/expertmode"
@@ -15,16 +22,24 @@ s = f:section(SimpleSection, nil, i18n.translate("MTU-Config-Text"))
 o = s:option(ListValue, "mtu", i18n.translate("MTU Block Size"))
 o.default = mtu
 o.rmempty = false
+
 for _, mtu in ipairs(site.fastd_mesh_vpn.mtu_posible_size.list) do
-  o:value(mtu, i18n.translate('ffffm-luci-set-mtu:mtu_size:' .. mtu))
+o:value(mtu,mtu)
 end
+o:value('auto', i18n.translate('ffffm-luci-set-mtu:mtu_size:' .. 'auto'))
 
 function f.handle(self, state, data)
   if state == FORM_VALID then
-    uci:set(config, "mesh_vpn", "mtu", data.mtu)
-
-    uci:save(config)
-    uci:commit(config)
+    if data.mtu == 'auto' then
+      uci:set(config, "mesh_vpn", "auto_mtu_enabled", 1)
+      uci:save(config)
+      uci:commit(config)
+    else
+      uci:set(config, "mesh_vpn", "mtu", data.mtu)
+      uci:set(config, "mesh_vpn", "auto_mtu_enabled", 0)
+      uci:save(config)
+      uci:commit(config)
+    end   
   end
 end
 
