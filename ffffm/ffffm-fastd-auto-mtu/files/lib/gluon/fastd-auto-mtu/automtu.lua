2590fd31539e10bc977df992f88af002a3813544
JuS <sagurna@s3i.de>
2016-02-17 03:32:07 +0100

    Auto-MTU: Schön gemacht

--- ffffm/ffffm-fastd-auto-mtu/files/lib/gluon/fastd-auto-mtu/automtu.lua
+++ ffffm/ffffm-fastd-auto-mtu/files/lib/gluon/fastd-auto-mtu/automtu.lua
@@ -4,42 +4,47 @@ local uci = require('luci.model.uci').cursor()
 local fastd_mtu_low = '1280'
 local fastd_mtu_high = '1426'
 local DSL_mtu_high = '1492' - '28'
+local ptarget = '8.8.8.8'
 
 local result = fastd_mtu_low
 local mtu
 
 if not (uci:get('fastd','mesh_vpn','enabled') == '1') then
+  print('mesh_vpn not enabled')
   os.execute('logger automtu: mesh_vpn not enabled.')
   os.exit()
 end
 
 if not (uci:get('fastd','mesh_vpn','auto_mtu_enabled') == '1') then
-  os.execute('logger automtu: automtu not enabled.')
+  print('auto_mtu not enabled')
+  os.execute('logger automtu: auto_mtu not enabled.')
   os.exit()
 end
 
 
 function setMTU ( x )
-  os.execute('logger automtu: fastd MTU changed')
-  os.execute('logger automtu: restart fastd')
   print('fastd MTU changed')
-  print('restart fastd...')
+  os.execute('logger automtu: fastd MTU changed')
   uci:set('fastd', 'mesh_vpn', 'mtu', x)
   uci:save('fastd')
   uci:commit('fastd')
+  print('restart fastd...')
+  os.execute('logger automtu: restart fastd')
   os.execute('/etc/init.d/fastd restart')
   return x
 end
 
-local f = io.popen("/usr/bin/ping -M do -s " ..DSL_mtu_high.. " -c 5 8.8.8.8")
+
+local f = io.popen('/usr/bin/ping -M do -s ' .. DSL_mtu_high .. ' -c 5 ' .. ptarget)
 local o = f:read('*all')
 f:close()
 
 if o:find('5 packets transmitted') then
   if not o:find('100%% packet loss') then
-    result=fastd_mtu_high
+    result = fastd_mtu_high
    end
 end
+
 
 mtu = uci:get('fastd', 'mesh_vpn', 'mtu')
 
@@ -53,6 +58,6 @@ else
   end
 end
 
+
 print ('fastd MTU = ' .. mtu .. ' Byte')
 os.execute('logger automtu: fastd MTU = ' .. mtu .. ' Byte')
-
