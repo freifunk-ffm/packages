f7ecbf6e34fda9674aab80a69d3dff4d40219e00
JuS <sagurna@s3i.de>
2016-02-27 02:42:40 +0100

    fastd-auto-mtu: fastd wird jetzt nicht mehr neu gestartet (das x86-Image mag das nicht)

--- ffffm/ffffm-fastd-auto-mtu/files/lib/gluon/fastd-auto-mtu/automtu.lua
+++ ffffm/ffffm-fastd-auto-mtu/files/lib/gluon/fastd-auto-mtu/automtu.lua
@@ -1,4 +1,5 @@
 #!/usr/bin/lua
+
 local uci = require('luci.model.uci').cursor()
 
 local fastd_mtu_low = '1280'
@@ -8,44 +9,52 @@ local ptarget = '8.8.8.8'
 
 local result = fastd_mtu_low
 local wan_if = 'br-wan'
-local x86_delay = '10'
+local delay = '10'
 local mtu
 
+local f
+local o
+
+-- 
+function setMTU ( x )
+  os.execute('logger automtu: new fastd MTU saved')
+  uci:set('fastd', 'mesh_vpn', 'mtu', x)
+  uci:save('fastd')
+  uci:commit('fastd')
+  os.execute('logger automtu: please reboot your device to activate the new MTU-Size...')
+  return x
+end
+
+--
+print('Fuer eine Ausgabe bitte "logread -f" auf einer anderen Konsole starten.')
+
+-- 
 if not (uci:get('fastd','mesh_vpn','enabled') == '1') then
-  print('mesh_vpn not enabled')
   os.execute('logger automtu: mesh_vpn not enabled.')
   os.exit()
 end
 
 if not (uci:get('fastd','mesh_vpn','auto_mtu_enabled') == '1') then
-  print('auto_mtu not enabled')
   os.execute('logger automtu: auto_mtu not enabled.')
   os.exit()
 end
 
+--
+mtu = uci:get('fastd', 'mesh_vpn', 'mtu')
+os.execute('logger automtu: current fastd MTU = ' .. mtu .. ' Byte')
 
-function setMTU ( x )
-  print('fastd MTU changed')
-  os.execute('logger automtu: fastd MTU changed')
-  uci:set('fastd', 'mesh_vpn', 'mtu', x)
-  uci:save('fastd')
-  uci:commit('fastd')
-  print('restart fastd...')
-  os.execute('logger automtu: restart fastd')
-  os.execute('/etc/init.d/fastd restart')
-  return x
-end
-
-
-local f = io.popen('/sbin/ifconfig ' .. wan_if)
-local o = f:read('*all')
+--
+os.execute('logger automtu: Check Up-Link...')
+f = io.popen('/sbin/ifconfig ' .. wan_if)
+o = f:read('*all')
 f:close()
-
 if not o:find('inet addr:') then
-  os.execute('sleep ' .. x86_delay)
+  os.execute('logger automtu: Up-Link not found yet. Wait...')
+  os.execute('sleep ' .. delay)
 end
 
-
+--
+os.execute('logger automtu: Start MTU-Check...')
 f = io.popen('/usr/bin/ping -M do -s ' .. DSL_mtu_high .. ' -c 5 ' .. ptarget)
 o = f:read('*all')
 f:close()
@@ -56,20 +65,16 @@ if o:find('5 packets transmitted') then
    end
 end
 
-
-mtu = uci:get('fastd', 'mesh_vpn', 'mtu')
-
+--
 if result == fastd_mtu_high then
   if not (mtu == fastd_mtu_high) then
-    mtu = setMTU (fastd_mtu_high)
+    mtu = setMTU (fastd_mtu_high, uplink)
   end
 else
   if not (mtu == fastd_mtu_low) then
-    mtu = setMTU (fastd_mtu_low)
+    mtu = setMTU (fastd_mtu_low, uplink)
   end
 end
 
-
-print ('fastd MTU = ' .. mtu .. ' Byte')
-os.execute('logger automtu: fastd MTU = ' .. mtu .. ' Byte')
-
+--
+os.execute('logger automtu: new fastd MTU = ' .. mtu .. ' Byte')
