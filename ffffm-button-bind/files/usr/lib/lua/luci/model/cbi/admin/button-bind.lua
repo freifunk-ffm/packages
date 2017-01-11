e069a5d1341c5469c24a9be7591c1f118d0fc35e
JuS <sagurna@s3i.de>
2017-01-11 18:12:19 +0100

    ffffm-button-bind: Umbauarbeiten, erster Schuss

--- ffffm-button-bind/files/usr/lib/lua/luci/model/cbi/admin/button-bind.lua
+++ ffffm-button-bind/files/usr/lib/lua/luci/model/cbi/admin/button-bind.lua
@@ -1,40 +1,30 @@
-local f, s, o
+local f, s, o, fct
 local site = require 'gluon.site_config'
 local i18n = require 'luci.i18n'
 local uci = luci.model.uci.cursor()
 
-f = SimpleForm('Button-Bind', i18n.translate('Button-Bind-Config-Titel'))
+f = SimpleForm('button-bind', i18n.translate('button-bind:Titel'))
 f.template = 'admin/expertmode'
+s = f:section(SimpleSection, nil, i18n.translate('button-bind:Text'))
 
-s = f:section(SimpleSection, nil, i18n.translate('Button-Bind-Text'))
-
--- https://github.com/openwrt/luci/blob/master/documentation/CBI.md
-
-o = s:option(ListValue, "wifi", i18n.translate('WiFi Button'))
-o.default = '0'
+-- Sollen mehrere Taser konfiguriert werden, dann einfach folgendes Schmeata vervielfaeltigen:
+fct = uci:get('button-bind', 'wifi', 'function')
+if not fct then
+fct='0'
+end
+o = s:option(ListValue, "wifi", i18n.translate('button-bind:WiFi-on-off'))
+o.default = fct
 o.widget = "radio"
 o.rmempty = false
-o:value('0', "Taster hat keine Funktion. (Grundeinstellung)")
-o:value('1', "Taster schaltet das WLAN an/aus. (OpenWrt-Funktionalitaet)")
-o:value('2', "Taster schaltet nur das Client-WLAN an/aus.")
-o:value('3', "Waehrend der Tasterbetaetigung werden in diesem Modus die generell abgeschalteten Status-LED zugeschaltet. Nach der Tasterbetaetigung werden die Status-LED wieder abgeschaltet.")
-
-o = s:option(ListValue, "reset", i18n.translate("Reset Button"))
-o.default = '0'
-o.widget = "radio"
-o.rmempty = false
-o:value('0', "Taster hat bekannte Gluon-Funktionalitaet. (Grundeinstellung)")
-o:value('1', "Taster hat keine Funktion.")
-
+o:value('0', i18n.translate('button-bind:fct_0'))
+o:value('1', i18n.translate('button-bind:fct_1'))
+o:value('2', i18n.translate('button-bind:fct_2'))
+o:value('3', i18n.translate('button-bind:fct_3'))
 function f.handle(self, state, data)
 	if state == FORM_VALID then
-		if data.wifi == '0' then
-		else
-		end
-		if data.reset == '0' then
-		else
-		end
+		uci:set('button-bind', 'wifi', 'function', data.wifi)
+		uci:save('button-bind')
 	end
 end
 
-return f 
+return f
