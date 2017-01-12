d5d75b8f16b34a12f801ab0c8bebcdce2431e109
JuS <sagurna@s3i.de>
2017-01-13 00:19:44 +0100

    ffffm-button-bind: Feinschliff

--- ffffm-button-bind/files/usr/lib/lua/luci/controller/admin/button-bind.lua
+++ ffffm-button-bind/files/usr/lib/lua/luci/controller/admin/button-bind.lua
@@ -1,4 +1,4 @@
 module("luci.controller.admin.button-bind", package.seeall)
 function index()
-        entry({"admin", "button-bind"}, cbi("admin/button-bind"), "Tasten", 85)
+        entry({"admin", "button-bind"}, cbi("admin/button-bind"), "Taster", 85)
 end
