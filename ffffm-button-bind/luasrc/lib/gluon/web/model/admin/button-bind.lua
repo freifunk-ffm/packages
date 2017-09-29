fe049de39be9e83953ab1ca8b65366829fdf0bd3
rubo77 <github@r.z11.de>
2017-09-29 16:06:02 +0200

    fffm-button-bind: selectbox instead of radio buttons

--- ffffm-button-bind/luasrc/lib/gluon/web/model/admin/button-bind.lua
+++ ffffm-button-bind/luasrc/lib/gluon/web/model/admin/button-bind.lua
@@ -1,32 +1,29 @@
 local uci = require("simple-uci").cursor()
 
-local f, s, o, fct
+local f = Form('Taster')
+local s = f:section(Section, nil, "Hier können dem Router-Taster unterschiedliche Funktionalitäten zugeordnet werden.")
 
-f = Form('Taster')
-s = f:section(Section, nil, "Hier können dem Router-Taster unterschiedliche Funktionalitäten zugeordnet werden.")
-
-fct = uci:get('button-bind', 'wifi', 'function')
+local fct = uci:get('button-bind', 'wifi', 'function')
 if not fct then
 	fct='0'
 	uci:set('button-bind', 'wifi', 'button')
 	uci:set('button-bind', 'wifi', 'function', fct)
 	uci:commit('button-bind')
 end
-o = s:option(ListValue, "wifi", "Wifi ON/OFF Taster")
+local o = s:option(ListValue, "wifi", "Wifi ON/OFF Taster")
 o.default = fct
-o.widget = "radio"
-o.rmempty = false
 o:value('0', "Wifi an/aus (default)")
 o:value('1', "Funktionslos")
 o:value('2', "Wifi-Reset")
 o:value('3', "alle Status-LEDs an/aus (Nachtmodus)")
 o:value('4', "Client-Netz an/aus")
 
-function f.handle(self, state, data)
-	if state == FORM_VALID then
-		uci:set('button-bind', 'wifi', 'function', data.wifi)
-		uci:commit('button-bind')
-	end
+function o:write(data)
+	uci:set('button-bind', 'wifi', 'function', data)
+end
+
+function f:write()
+	uci:commit('button-bind')
 end
 
 return f
