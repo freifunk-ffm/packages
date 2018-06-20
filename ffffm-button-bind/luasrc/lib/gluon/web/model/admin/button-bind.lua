6d5a64272bef00c5c9d7f53969c990695975950e
JuS <sagurna@s3i.de>
2018-06-20 22:00:12 +0200

    ffffm-button-bind: Rückbau und Anpassungen an Gluon 2017.1.x

---
+++ ffffm-button-bind/luasrc/lib/gluon/web/model/admin/button-bind.lua
@@ -0,0 +1,33 @@
+local uci = require("simple-uci").cursor()
+
+local f = Form('Taster')
+local s = f:section(Section, nil, "Hier können dem Router-Taster unterschiedliche Funktionalitäten zugeordnet werden.")
+
+
+-- Sollen mehrere Taster konfiguriert werden, dann einfach folgendes Schemata vervielfaeltigen:
+
+local fct = uci:get('button-bind', 'wifi', 'function')
+if not fct then
+	fct='0'
+	uci:set('button-bind', 'wifi', 'button')
+	uci:set('button-bind', 'wifi', 'function', fct)
+	uci:commit('button-bind')
+end
+local o = s:option(ListValue, "wifi", "Wifi ON/OFF Taster")
+o.default = fct
+o:value('0', "Wifi an/aus (default)")
+o:value('1', "Funktionslos")
+o:value('2', "Wifi-Reset")
+o:value('3', "In diesem Modus sind die Status-LEDs generell deaktiviert. Während der Tasterbetätigung werden die Status-LEDs temporär aktiviert.")
+
+function o:write(data)
+	uci:set('button-bind', 'wifi', 'function', data)
+end
+
+function f:write()
+	uci:commit('button-bind')
+end
+
+return f
+
+
