960b6ac7e7db5fd19c9821e2b847fb049370f390
JuS <sagurna@s3i.de>
2020-12-02 17:29:25 +0100

    Revert "ath9k-broken-wifi-workaround: Statt eines Wifi-Scans jetzt sofort ein Wifi-Restart."
    This reverts commit 35ffd19204c4bf9966afa321b34be39f93578b24.

--- ffffm-ath9k-broken-wifi-workaround/files/lib/gluon/ath9k-broken-wifi-workaround/ath9k-broken-wifi-workaround.sh
+++ ffffm-ath9k-broken-wifi-workaround/files/lib/gluon/ath9k-broken-wifi-workaround/ath9k-broken-wifi-workaround.sh
@@ -320,8 +320,12 @@ fi
 
 if [ ! -f "$RESTARTFILE" ] && [ "$WIFIRESTART" -eq 1 ]; then
 	touch $RESTARTFILE
-# 	Jetzt ein Wifi-Treiber-Restart
-	/sbin/wifi
+# 	Sicherheitshalber mal ein Scan als Behelf absetzen.
+# 	Schadet ja nicht...
+#   Eine ggf. doppelte Behandlung stoert nicht. 
+	for wifidev in $ATH9K_IFS; do
+		/usr/sbin/iw dev $wifidev scan
+	done
 	multilog "Wifi restart is pending"
 elif [ $WIFIRESTART -eq 1 ]; then
 	multilog "*** Wifi restarted ***"
@@ -331,6 +335,11 @@ elif [ $WIFIRESTART -eq 1 ]; then
 	rm -rf $GWFILE
 	rm -rf $RESTARTFILE
 # 	Jetzt ein Wifi-Treiber-Restart
+# 	Doppeltgemoppelt haelt besser. Daher erst ein 'iw scan' gefolgt von einem 'wifi'
+#   Eine ggf. doppelte Behandlung stoert nicht.
+	for wifidev in $ATH9K_IFS; do
+		/usr/sbin/iw dev $wifidev scan
+	done
 	/sbin/wifi
 else
 # 	systemlog "Everything seems to be ok"
